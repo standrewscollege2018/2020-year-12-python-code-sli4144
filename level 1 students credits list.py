fa12601454e043b5a2fbba8f9596d98bad0f0cb2
sli4144 <60494670+sli4144@users.noreply.github.com>
2020-02-14 15:14:09 +1300

    list in list, add credits with the name of each student in one small list which store in the bigger list.

--- level 1 students credits list.py
+++ level 1 students credits list.py
@@ -8,10 +8,10 @@ def display_all_students():
 
 
 #manaage student info about L1 ncea credits
-#store student detail
-names = ["Amy","Andy","Ann","Bob","John"]
+#store student detail names and scores
+names = [["Amy",35], ["Andy",89], ["Ann",70], ["Bob",23], ["John",67]]
 # another list which store the credits of the corresponding student
-credits = [35,89,74,110,29]
+#3credits = [35,89,74,110,29]
 
 #display all the names in the list using a for loop
 #for i in range(0, len(names)):
@@ -21,23 +21,28 @@ display_all_students()
 
 # function that able to add student into the name list
 name = input("add a student:")
-names.append (name)
+credit = int(input("what is the number of the credit:"))
+new_student = [name,credit]
+names.append (new_student)
 #call function
-display_all_students
+display_all_students()
 
 
 # function that able to delete student
-name = input("enter a index of student to delete")
-del (student(name))
+name = int(input("enter the number of the student you want to delete: "))
+del names[name-1]
 # call function
 display_all_students()
 
 
 #change the detail of the student
 #get details of which student to change and what the updated name is
-name = input("enter the name of the student you want to change:")
+student_number = int(input("enter the number of the student you want to change:"))
 new_name = input("enter the new name of the student:")
-nane = new_name
+
+#change student details
+names[student_number - 1] = new_name
+
 # call function
 display_all_students()
 
