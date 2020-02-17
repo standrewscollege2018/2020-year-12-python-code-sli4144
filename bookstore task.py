d2edb75be765ca229f2ad1c6290c51f4c82184b8
sli4144 <60494670+sli4144@users.noreply.github.com>
2020-02-18 09:29:21 +1300

    for loop print each item in the list

--- bookstore task.py
+++ bookstore task.py
@@ -1,2 +1,6 @@
 #list store all the book detail
-books= [[In to the wild, Jon Krakauer, 20.3], [Castle, Franz Kafka, 16.5],[The three body problem, Liu Cixin, 50.25]]
+books= [["In to the wild", "Jon Krakauer", 20.3], ["Castle", "Franz Kafka", 16.5],["The three body problem", "Liu Cixin", 50.25]]
+
+# a loop go through the list and print out each item
+for book in range(0, len(books)):
+    print (book+1, books[book])
