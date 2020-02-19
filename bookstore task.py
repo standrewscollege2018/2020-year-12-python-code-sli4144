e08133d2ff2bb2776a01eded580e57c0091d8527
sli4144 <60494670+sli4144@users.noreply.github.com>
2020-02-20 09:19:45 +1300

    function all the function

--- bookstore task.py
+++ bookstore task.py
@@ -3,29 +3,35 @@ books= [["In to the wild", "Jon Krakauer", 20.3], ["Castle", "Franz Kafka", 16.5
 
 # function 
 # a loop go through the list and print out each item
-def all_books():
+def show_books():
     for book in range(0, len(books)):
         print (book+1, books[book])
 #call function
-all_books()
+show_books()
 
-# add a new book into the list
-newtitle = input("enter the title of the new book:")
-newauthor = input("enter the author of the new book:")
-newprice = float(input("enter the price of the new book:"))
-new_book = [newtitle, newauthor, newprice]
-books.append (new_book)
+# add function
+def add():
+    # add a new book into the list
+    newtitle = input("enter the title of the new book:")
+    newauthor = input("enter the author of the new book:")
+    newprice = float(input("enter the price of the new book:"))
+    new_book = [newtitle, newauthor, newprice]
+    books.append (new_book)
+    
+# delete function
+def delete():
+    # delete a book 
+    book = int(input("enter the number of the book you want to delete:"))
+    del books[book-1]
 
-# delete a book 
-book = int(input("enter the number of the book you want to delete:"))
-del books[book-1]
-
-#update a book detail
-update = int(input("enter the number of book you want to update:"))
-updatetitle = input("update the title:")
-updateauther = input("update the auther:")
-updateprice = float(input("update the price:"))
-update_book = [updatetitle, updateauther, updateprice]
-books[update-1] = update_book
+# update function
+def update():
+    #update a book detail
+    update = int(input("enter the number of book you want to update:"))
+    updatetitle = input("update the title:")
+    updateauther = input("update the auther:")
+    updateprice = float(input("update the price:"))
+    update_book = [updatetitle, updateauther, updateprice]
+    books[update-1] = update_book
 #call function
-all_books()
+show_books()
