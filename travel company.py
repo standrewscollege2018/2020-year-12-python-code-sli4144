c1d8292a1dd2b583a9293ccbb38db43ed455e140
sli4144 <60494670+sli4144@users.noreply.github.com>
2020-03-03 08:46:27 +1300

    Create travel company.py

---
+++ travel company.py
@@ -0,0 +1,23 @@
+#list store the departure location
+depart_loc = ["1. Auckland", "2. Wellington","3. Christchurch"]
+
+#list store the destination
+destin_loc = ["1.Sydney", "2.Tonga", "3.Shanghai", " 4.London"]
+
+#list store the price of the destination
+price = ["1.Sydney - $326", "2.Tonga - $378", "3.Shanghai - $1436", " 4.London - $2376 "]
+
+#list store all the functionality of the programm
+program_funct = ["1.departure location", "2.destination", "3. price of the destination", "4.book your flight", "5.exit"]
+
+# functions 
+# funtion that show all the departure location
+def de():
+    print (depart_loc)
+# function that show all the destination 
+def 
+
+
+print ("welcome to the programm, what can we help?")
+print (program_funct)
+do = int(input("enter the number of function helps you:"))
