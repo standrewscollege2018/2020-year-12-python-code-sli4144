f82df16e67e0088c220cdcbef3a272e70b618e85
sli4144 <60494670+sli4144@users.noreply.github.com>
2020-03-03 09:34:43 +1300

    Update travel company.py

--- travel company.py
+++ travel company.py
@@ -10,14 +10,50 @@ price = ["1.Sydney - $326", "2.Tonga - $378", "3.Shanghai - $1436", " 4.London -
 #list store all the functionality of the programm
 program_funct = ["1.departure location", "2.destination", "3. price of the destination", "4.book your flight", "5.exit"]
 
-# functions 
+
+
+
+
+          # functions 
 # funtion that show all the departure location
-def de():
-    print (depart_loc)
+def show_dep():
+    for x in range(len(depart_loc)):
+        print (depart_loc[x])
+
 # function that show all the destination 
-def 
+def show_des():
+    for i in range(len(destin_loc)):
+        print (destin_loc[i])
+
+# function that show price list
+def show_price():
+    for p in range(len(price)):
+        print(price[p])
+        
+# book tickets function
+def ticket_booking():
+    departure = int(input("enter the index of your departure location:"))
+    destination = int(input("enter the index of your detination:"))
+    nights = int(input("how many nights are you planning to stay?"))
+    print("Your flights are departure in", depart_loc[departure+1],", and your destination is", destin_loc[destination+1],"and you booked", nights,"nights.")
+    ticket_detail = [departure, destinaiton,nights]
+    
+    
+#cost calculation
+def cal_cost():
+    
+
+
+
+
+
+
+
+
+
 
 
 print ("welcome to the programm, what can we help?")
 print (program_funct)
-do = int(input("enter the number of function helps you:"))
+do = int(input("enter the number of function helps you:")):
+    if do == 
